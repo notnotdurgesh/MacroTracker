c028bea6cd5230b6d18c35471b7b7d9124520ecd
Reddy Durgeshwant <74865452+notnotdurgesh@users.noreply.github.com>
2024-07-22 00:34:00 +0530

    Update server.js

--- backend-js/server.js
+++ backend-js/server.js
@@ -31,6 +31,11 @@ app.use((err, req, res, next) => {
     res.status(500).json({ success: false, error: 'Internal Server Error' });
 });
 
+app.get('/heartbeat', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
